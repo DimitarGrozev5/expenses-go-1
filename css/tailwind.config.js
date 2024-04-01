1dcc7013102634926f1b7fbdd0a62fb7a8bdac89
DimitarGrozev5 <95020107+DimitarGrozev5@users.noreply.github.com>
2024-04-01 23:16:05 +0300

    set primary color in tailwind config

--- css/tailwind.config.js
+++ css/tailwind.config.js
@@ -1,9 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["../templates/**/*.htm", "./**/*.css"],
+  content: ["../templates/**/*.htm", "./**/*.css", "../static/html/**/*.htm"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-50": "#fafafa",
+        "primary-100": "#f4f4f5",
+        "primary-200": "#e4e4e7",
+        "primary-300": "#d4d4d8",
+        "primary-400": "#a1a1aa",
+        "primary-500": "#71717a",
+        "primary-600": "#52525b",
+        "primary-700": "#3f3f46",
+        "primary-800": "#27272a",
+        "primary-900": "#18181b",
+        "primary-950": "#09090b",
+      },
+    },
   },
   plugins: [],
-}
-
+};
