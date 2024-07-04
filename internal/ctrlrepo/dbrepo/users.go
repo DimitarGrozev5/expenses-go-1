ac7fdb22b0b32801d321c800955b336a60ca6647
DimitarGrozev5 <95020107+DimitarGrozev5@users.noreply.github.com>
2024-07-04 22:22:19 +0300

    create user in db

--- internal/ctrlrepo/dbrepo/users.go
+++ internal/ctrlrepo/dbrepo/users.go
@@ -51,5 +51,32 @@ func (m sqliteDBRepo) GetMaxUserVersion() (int64, error) {
 }
 
 func (m sqliteDBRepo) AddNewUser(email string, version int64) error {
+	// Define context with timeout
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	// Start transaction
+	tx, err := m.DB.Begin()
+	if err != nil {
+		return err
+	}
+	defer tx.Rollback()
+
+	// Set query
+	stmt := `INSERT INTO users (user_email, db_version, status) VALUES ($1, $2, 1)`
+
+	// Execute query
+	_, err = tx.ExecContext(
+		ctx,
+		stmt,
+		email,
+		version,
+	)
+	if err != nil {
+		return err
+	}
+
+	tx.Commit()
+
 	return nil
 }
